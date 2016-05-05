bbf2feec330db3c7792075924e9f2414c31851cb
Doni Ramadhan <doniramadhan@gmail.com>
2016-05-05 08:48:53 +0200

    add unit test for addMember, removeMember, removeAllMembers, countMembers add init for Group

--- model/group.py
+++ model/group.py
@@ -7,14 +7,25 @@
 class Group:
     members = []
 
+    def __init__(self):
+        self.members = []
+
     def addMember(self, username):
-        self.members.append(username)
+        if username not in self.members:
+            self.members.append(username)
+        else:
+            # TODO raise custom exception for adding existing member
+            pass
 
     def removeMember(self, username):
-        self.members.remove(username)
+        if username in self.members:
+            self.members.remove(username)
+        else:
+            # TODO raise custom exception for removing non-existing member
+            pass
 
     def removeAllMembers(self):
         self.members = []
 
     def countMembers(self):
-        return self.members.count()
+        return len(self.members)
